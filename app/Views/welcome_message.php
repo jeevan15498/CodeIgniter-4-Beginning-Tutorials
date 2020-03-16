a53b2854f36a9f01f6be9dad8f21734c78463996
Jeevan Lal <jeevan15498@gmail.com>
2020-03-16 11:11:29 +0530

    Install and Setup CodeIgniter 4

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to CodeIgniter 4!</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+</head>
+<body>
+<h2>Welcome to CodeIgniter 4</h2>
+</body>
+</html>
